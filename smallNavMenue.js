a1078722142f1223c99ff7a5a20c815bf20e35c2
Zeshan Haider <zeshan.haider@emumba.com>
2022-09-08 11:21:40 -0600

    Completed but, still I've got some small issues that need to be resolved.

---
+++ smallNavMenue.js
@@ -0,0 +1,10 @@
+
+function runFunction() {
+    var x = document.getElementById("smallNavMenue");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
